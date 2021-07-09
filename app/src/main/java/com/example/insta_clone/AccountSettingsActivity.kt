aec601088376a30d13fa54264668c88207b015d1
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-09 20:21:01 +0530

    updated edit profile
    Took 1 hour 19 minutes

---
+++ app/src/main/java/com/example/insta_clone/AccountSettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.insta_clone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_settings)
+    }
+}
