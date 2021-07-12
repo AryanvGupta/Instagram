7ac7eb46392200780e27478158d86d9ae79ab6e8
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-12 22:46:34 +0530

    user Info and profile image modifications
    Took 1 hour 9 minutes

--- app/src/main/java/com/example/insta_clone/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/insta_clone/AccountSettingsActivity.kt
@@ -1,29 +1,44 @@
 package com.example.insta_clone
 
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.insta_clone.Model.User
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_account_settings.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class AccountSettingsActivity : AppCompatActivity() {
     private lateinit var firebaseUser: FirebaseUser
     private var checker = ""
+    private var myUrl = ""
+    private var imageUri: Uri? = null
+    private var storageProfilePicRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
 
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
+        storageProfilePicRef = FirebaseStorage.getInstance().reference.child("Profile Pictures")
 
         logout_btn.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -34,9 +49,17 @@ class AccountSettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        change_image_text_btn.setOnClickListener {
+            checker = "clicked"
+
+            CropImage.activity()
+                .setAspectRatio(1, 1)
+                .start(this@AccountSettingsActivity)
+        }
+
         save_info_profile_btn.setOnClickListener {
             if (checker == "clicked"){
-
+                uploadImageAndUpdateInfo()
             }
             else {
                 updateUserInfoOnly()
@@ -44,6 +67,73 @@ class AccountSettingsActivity : AppCompatActivity() {
         }
 
         userInfo()
+    }
+
+    private fun uploadImageAndUpdateInfo() {
+
+
+        when {
+            imageUri == null -> Toast.makeText(this, "Please select a valid image.", Toast.LENGTH_LONG).show()
+            full_name_profile_frag.text.toString() == "" || user_name_profile_frag.text.toString() == "" -> {
+                Toast.makeText(this, "Please fill all fields.", Toast.LENGTH_LONG).show()
+            }
+            else -> {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Account Settings")
+                progressDialog.setMessage("Please wait, we are updating your profile...")
+                progressDialog.show()
+
+                val fileRef = storageProfilePicRef!!.child(firebaseUser!!.uid + ".jpg")
+
+                var uploadTask: StorageTask<*>
+                uploadTask = fileRef.putFile(imageUri!!)
+                uploadTask.continueWithTask(Continuation <UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                    if (!task.isSuccessful){
+                        task.exception?.let {
+                            throw it
+                            progressDialog.dismiss()
+                        }
+                    }
+                    return@Continuation fileRef.downloadUrl
+                }).addOnCompleteListener ( OnCompleteListener<Uri> { task ->
+                    if (task.isSuccessful) {
+                        val downloadUrl = task.result
+                        myUrl = downloadUrl.toString()
+
+                        val ref = FirebaseDatabase.getInstance().reference.child("Users")
+
+                        val userMap = HashMap<String, Any>()
+
+                        userMap["fullname"] = full_name_profile_frag.text.toString().toLowerCase()
+                        userMap["username"] = user_name_profile_frag.text.toString().toLowerCase()
+                        userMap["bio"] = bio_profile_frag.text.toString().toLowerCase()
+                        userMap["image"] = myUrl
+
+                        ref.child(firebaseUser.uid).updateChildren(userMap)
+
+                        val intent = Intent(this@AccountSettingsActivity, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                        progressDialog.dismiss()
+                    }
+                    else {
+                        progressDialog.dismiss()
+                    }
+                })
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null){
+            val result = CropImage.getActivityResult(data)
+            imageUri = result.uri
+            profile_image_view_profile_frag.setImageURI(imageUri)
+        }
     }
 
     private fun updateUserInfoOnly() {
@@ -67,9 +157,6 @@ class AccountSettingsActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
-
-
     }
 
     private fun userInfo() {
