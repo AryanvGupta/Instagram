e2901463d75c095412ef3c2bb829176f2ac142dd
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-13 20:28:29 +0530

    Upload post, Like post, view post of following users
    Took 1 hour 27 minutes

---
+++ app/src/main/java/com/example/insta_clone/Adapter/PostAdapter.kt
@@ -0,0 +1,88 @@
+package com.example.insta_clone.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.insta_clone.Model.Post
+import com.example.insta_clone.Model.User
+import com.example.insta_clone.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+class PostAdapter (private val mContext: Context,
+                   private val mPost: List<Post>): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    private var firebaseUser: FirebaseUser? = null
+
+    inner class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var profileImage: CircleImageView
+        var postImage: ImageView
+        var likeButton: ImageView
+        var commentButton: ImageView
+        var saveButton: ImageView
+        var userName: TextView
+        var likes: TextView
+        var publisher: TextView
+        var description: TextView
+        var comments: TextView
+
+        init {
+            profileImage = itemView.findViewById(R.id.user_profile_image_post)
+            postImage = itemView.findViewById(R.id.post_image_home)
+            likeButton = itemView.findViewById(R.id.post_image_like_btn)
+            commentButton = itemView.findViewById(R.id.post_image_comment_btn)
+            saveButton = itemView.findViewById(R.id.post_save_comment_btn)
+            userName = itemView.findViewById(R.id.user_name_post)
+            likes = itemView.findViewById(R.id.likes)
+            publisher = itemView.findViewById(R.id.publisher)
+            description = itemView.findViewById(R.id.description)
+            comments = itemView.findViewById(R.id.comments)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.posts_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        val post = mPost[position]
+        Picasso.get().load(post.getPostimage()).into(holder.postImage)
+        publisherInfo(holder.profileImage, holder.userName, holder.publisher, post.getPublisher())
+    }
+
+    private fun publisherInfo(profileImage: CircleImageView, userName: TextView, publisher: TextView, publisherID: String) {
+        val usersRef = FirebaseDatabase.getInstance().reference.child("Users").child(publisherID)
+        usersRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(pO: DataSnapshot) {
+                if (pO.exists()) {
+                    val user = pO.getValue<User>(User::class.java)
+
+                    Picasso.get().load(user!!.getImage()).placeholder(R.drawable.profile).into(profileImage)
+                    userName.text = user!!.getUsername()
+                    publisher.text = user!!.getFullname()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return mPost.size
+    }
+}
--- app/src/main/java/com/example/insta_clone/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/insta_clone/fragment/HomeFragment.kt
@@ -7,7 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.insta_clone.Adapter.PostAdapter
+import com.example.insta_clone.Model.Post
 import com.example.insta_clone.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +27,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    private var postAdapter: PostAdapter? = null
+    private var postList: MutableList<Post>? = null
+    private var followingList: MutableList<Post>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +51,60 @@ class HomeFragment : Fragment() {
         linearLayoutManager.stackFromEnd = true
         recyclerView.layoutManager = linearLayoutManager
 
+        postList = ArrayList()
+        postAdapter = context?.let { PostAdapter(it, postList as ArrayList<Post>) }
+        recyclerView.adapter = postAdapter
+
+        checkFollowings()
+
         return view
+    }
+
+    private fun checkFollowings() {
+        followingList = ArrayList()
+
+        val followingRef = FirebaseDatabase.getInstance().reference
+                .child("Follow").child(FirebaseAuth.getInstance().currentUser!!.uid)
+                .child("Following")
+
+        followingRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(pO: DataSnapshot) {
+                if (pO.exists()) {
+                    (followingList as ArrayList<String>).clear()
+                    for (snapshot in pO.children) {
+                        snapshot.key?.let { (followingList as ArrayList<String>).add(it) }
+                    }
+
+                    retrievePosts()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    private fun retrievePosts() {
+        val postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
+
+        postsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(pO: DataSnapshot) {
+                postList?.clear()
+
+                for (snapshot in pO.children) {
+                    val post = snapshot.getValue(Post::class.java)
+
+                    for (id in (followingList as ArrayList<String>)) {
+                        if (post!!.getPublisher() == id) {
+                            postList!!.add(post)
+                        }
+
+                        postAdapter!!.notifyDataSetChanged()
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 
 //    companion object {
