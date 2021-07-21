0dc076cfeef22089261897bc203388fda51051db
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-21 13:13:20 +0530

    commentAdapter
    Took 15 minutes

--- app/src/main/java/com/example/insta_clone/Adapter/CommentsAdapter.kt
+++ app/src/main/java/com/example/insta_clone/Adapter/CommentsAdapter.kt
@@ -1,27 +1,64 @@
 package com.example.insta_clone.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.example.insta_clone.Model.Comment
+import com.example.insta_clone.Model.User
 import com.example.insta_clone.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 
 class CommentsAdapter (private val mContext: Context,
                        private val mComment: MutableList<Comment>?) : RecyclerView.Adapter<CommentsAdapter.ViewHolder>() {
+
+    private var firebaseUser: FirebaseUser? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentsAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(mContext).inflate(R.layout.comments_item_layout, parent, false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CommentsAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        val comment = mComment!![position]
+        holder.commentTV.text = comment.getComment()
+
+        getUserInfo(holder.imageProfile, holder.userNameTV, comment.getPublisher())
+    }
+
+    private fun getUserInfo(imageProfile: CircleImageView, userNameTV: TextView, publisher: String) {
+        val userRef = FirebaseDatabase.getInstance().reference
+            .child("Users")
+            .child(publisher)
+
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(pO: DataSnapshot) {
+                if (pO.exists()) {
+                    val user = pO.getValue(User::class.java)
+                    Picasso.get().load(user!!.getImage()).placeholder(R.drawable.profile).into(imageProfile)
+                    userNameTV.text = user!!.getUsername()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mComment!!.size
     }
 
     inner class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
