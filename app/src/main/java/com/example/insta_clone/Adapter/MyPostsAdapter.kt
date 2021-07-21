9ed463db1ae6c6f63b741ada9b695dfd0a38ed78
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-21 14:52:23 +0530

    display user post module
    Took 5 minutes

---
+++ app/src/main/java/com/example/insta_clone/Adapter/MyPostsAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.insta_clone.Adapter
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.example.insta_clone.Model.Post
+
+class MyPostsAdapter (private val mContext: Context, mPost: List<Post>) : RecyclerView.Adapter<MyPostsAdapter.ViewHolder?>() {
+
+}
