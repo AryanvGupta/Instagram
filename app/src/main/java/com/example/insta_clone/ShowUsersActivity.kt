64d3035d1ad4aef7011eeb916adbbe26bc8bd0d8
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-21 22:07:00 +0530

    view users list module
    Took 2 minutes

---
+++ app/src/main/java/com/example/insta_clone/ShowUsersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.insta_clone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_users)
+    }
+}
