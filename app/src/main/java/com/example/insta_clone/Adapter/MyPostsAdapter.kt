33a81f64689d8d6b3499e2e2863b82ec6dcb5aaa
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-21 15:04:07 +0530

    display user post adapter module
    Took 7 minutes

--- app/src/main/java/com/example/insta_clone/Adapter/MyPostsAdapter.kt
+++ app/src/main/java/com/example/insta_clone/Adapter/MyPostsAdapter.kt
@@ -1,9 +1,36 @@
 package com.example.insta_clone.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.example.insta_clone.Model.Post
+import com.example.insta_clone.R
 
 class MyPostsAdapter (private val mContext: Context, mPost: List<Post>) : RecyclerView.Adapter<MyPostsAdapter.ViewHolder?>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.posts_item_layout, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return mPost!!.size
+    }
+
+    inner class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var postImage: ImageView
+
+        init {
+            postImage = itemView.findViewById(R.id.post_image)
+        }
+    }
 }
