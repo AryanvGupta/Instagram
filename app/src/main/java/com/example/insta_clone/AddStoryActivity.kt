ff8bb7838bacd40471780715cf0e571f62e2c576
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-07-23 21:35:08 +0530

    story addStory activity created module
    Took 4 minutes

---
+++ app/src/main/java/com/example/insta_clone/AddStoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.insta_clone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
